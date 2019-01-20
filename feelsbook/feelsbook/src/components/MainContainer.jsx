313e25b4193ff3e1acba22c689787522d8d88159
LionKingSimba <ttran1@ualberta.ca>
2019-01-19 20:56:00 -0700

    Add text boxes and header to maincontainer

--- feelsbook/feelsbook/src/components/MainContainer.jsx
+++ feelsbook/feelsbook/src/components/MainContainer.jsx
@@ -3,49 +3,37 @@ import './css/MainContainer.css';
 import { Card } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
 class MainContainer extends Component {
   render() {
     return (
-      <div className="MainContainer notebook">
-        {/* <Card>
-          <Card.Header>
-            <Nav variant="tabs" defaultActiveKey="#first">
-              <Nav.Item>
-                <Nav.Link href="#first">Active</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="#link">Link</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="#disabled" disabled>
-                  Disabled
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Card.Header>
-          <Card.Body>
-            <Card.Title>Special title treatment</Card.Title>
-            <Card.Text>
-              With supporting text below as a natural lead-in to additional content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>; */}
-
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>;
-      </div>
-
+      <Card className='MainContainer'>
+        <Card.Header>
+          <Nav variant="tabs" defaultActiveKey="#first">
+            <Nav.Item>
+              <Nav.Link href="#first">Write</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#link">History</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#disabled">Statistics</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Header>
+        <Card.Body>
+          <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Control placeholder="Title" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Control as="textarea" rows="3" placeholder="Write Here!"/>
+            </Form.Group>
+          </Form>
+          <Button variant="primary">Save Entry</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
