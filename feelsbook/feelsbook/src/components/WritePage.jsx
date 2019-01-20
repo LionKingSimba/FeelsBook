e33e2ff74b5e50d49299a2ec058cc5bae51880e8
LionKingSimba <ttran1@ualberta.ca>
2019-01-20 12:00:47 -0700

    Change background colors when saving

--- feelsbook/feelsbook/src/components/WritePage.jsx
+++ feelsbook/feelsbook/src/components/WritePage.jsx
@@ -49,6 +49,28 @@ class WritePage extends Component {
                 } else {
                     console.log('tone endpoint:');
                     console.log(JSON.stringify(tone, null, 2));
+                    console.log('length: ' + tone.document_tone.tones.length);
+                    for (var i = 0; i < tone.document_tone.tones.length; i++) {
+                        var tone_id = tone.document_tone.tones[i].tone_id;
+                        console.log("mood: " + tone.document_tone.tones[i]);
+                        console.log("Tone score: " + tone.document_tone.tones[i].score);
+                        console.log("Tone id: " + tone.document_tone.tones[i].tone_id);
+                        if (tone_id === 'anger') {
+                            document.body.style.backgroundColor = "red";
+                        } else if (tone_id === 'fear') {
+                            document.body.style.backgroundColor = "black";
+                        } else if (tone_id === 'joy') {
+                            document.body.style.backgroundColor = "yellow";
+                        } else if (tone_id === 'sadness') {
+                            document.body.style.backgroundColor = "blue";
+                        } else if (tone_id === 'analytical') {
+                            document.body.style.backgroundColor = "teal";
+                        } else if (tone_id === 'confident') {
+                            document.body.style.backgroundColor = "orange";
+                        } else if (tone_id === 'tentative') {
+                            document.body.style.backgroundColor = "pink";
+                        }
+                    }
                 }
             }
         );
