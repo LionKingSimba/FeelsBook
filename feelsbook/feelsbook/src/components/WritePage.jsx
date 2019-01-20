169341ddfc0aa6f375b25be13ece7a4e5676cd0e
LionKingSimba <ttran1@ualberta.ca>
2019-01-20 06:36:39 -0700

    Make Watson proxy server

--- feelsbook/feelsbook/src/components/WritePage.jsx
+++ feelsbook/feelsbook/src/components/WritePage.jsx
@@ -4,8 +4,52 @@ import { Button } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
+// import axios from 'axios';
+
+const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
+const keyfile = require('../key.json');
+
+const toneAnalyzer = new ToneAnalyzerV3({
+    version: '2017-09-21',
+    iam_apikey: keyfile.key,
+    url: 'https://gateway.watsonplatform.net/tone-analyzer/api'
+});
 
 class WritePage extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            currentTab: 'write',
+            moodText: ""
+        }
+    } 
+
+    getMoods = () => {
+        console.log('hello world');
+        console.log(this.state.moodText);
+        const toneVar = toneAnalyzer.tone(
+            {
+                tone_input: 'Greetings from the Watson Developer Cloud Node SDK, we are pleased to say hello!',
+                content_type: 'text/plain'
+            },
+             (err, tone) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('tone endpoint:');
+                    console.log(JSON.stringify(tone, null, 2));
+                }
+            }
+        );
+    }
+    
+    handleChange = (e) => {
+        this.setState({ 
+            moodText: e.target.value 
+        });
+    }
 
     render() {
         return (
@@ -16,12 +60,19 @@ class WritePage extends Component {
                             <Form.Control placeholder="Title" className="light-transparency" />
                         </Col>
                         <Col sm={2}>
-                            <Button variant="primary">Save Entry</Button>
+                            <Button variant="primary" onClick={this.getMoods}>Save Entry</Button>
                         </Col>
                     </Row>
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Control as="textarea" rows="15" placeholder="Write Here!" className="light-transparency" />
+                    <Form.Control 
+                        as="textarea" 
+                        rows="15" 
+                        placeholder="Write Here!" 
+                        className="light-transparency"
+                        value={this.state.moodText}
+                        onChange={this.handleChange}
+                        />
                 </Form.Group>
             </Form>
         );
--- feelsbook/feelsbook/watson-scripts/watson.js
+++ feelsbook/feelsbook/watson-scripts/watson.js
@@ -1,12 +1,65 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3001;
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+var keyfile = require('./key.json');
+
+var toneAnalyzer = new ToneAnalyzerV3({
+    version: '2017-09-21',
+    iam_apikey: keyfile.key,
+    url: 'https://gateway.watsonplatform.net/tone-analyzer/api'
+});
+
+const server = http.createServer((request, response) => {
+    const { headers, method, url } = request;
+    let body = [];
+    request.on('error', (err) => {
+        console.error(err);
+    }).on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        // At this point, we have the headers, method, url and body, and can now
+        // do whatever we need to in order to respond to this request.
+        console.log("BODY: " + body)
+        // BEGINNING OF NEW STUFF
+
+        var moodAnalysis = {};
+
+        toneAnalyzer.tone(
+            {
+                tone_input: 'Greetings from the Watson Developer Cloud Node SDK, we are pleased to say hello!',
+                content_type: 'text/plain'
+            },
+            function (err, tone) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('tone endpoint:');
+                    console.log(JSON.stringify(tone, null, 2));
+                    
+                    response.on('error', (err) => {
+                        console.error(err);
+                    });
+
+                    response.statusCode = 200;
+                    response.setHeader('Content-Type', 'application/json');
+
+                    const responseBody = { headers, method, url, body, tone };
+
+                    response.write(JSON.stringify(responseBody));
+                    response.end();
+                    // Note: the 2 lines above could be replaced with this next one:
+                    // response.end(JSON.stringify(responseBody))
+
+                    // END OF NEW STUFF
+                }
+            }
+        );
+
+    })
 });
 
 server.listen(port, hostname, () => {
